71a73519486f0d97796b4b55e026f9be561820d5
Peace <orlahtee@gmail.com>
2024-09-19 13:40:03 +0100

    turned pet card into a JS template

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ async function start() {
   
   start()
 
+  async function petsArea() {
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsData = await petsPromise.json()
+    petsData.forEach(pet => {
+        const clone = template.content.cloneNode(true)
+    
+        clone.querySelector("h3").textContent = pet.name
+    
+        wrapper.appendChild(clone)
+      })
+      document.querySelector(".list-of-pets").appendChild(wrapper)
+    }
+    
+    petsArea()
