1dd897f801751a004bc09f1e119b416c286be2c0
Peace <orlahtee@gmail.com>
2024-09-20 11:14:57 +0100

    Updated Javascript for added  fallback image

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
 async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
     const weatherData = await weatherPromise.json()
